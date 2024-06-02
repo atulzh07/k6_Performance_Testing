956c90f7f072262ad0547ffeb12c9bd7eb469506
atulzh07 <atulzh7@protonmail.com>
2024-06-03 00:21:17 +0545

    feat: added assertions for k6 load tests

---
+++ scripts/assertions.js
@@ -0,0 +1,13 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export default function () {
+  const response = http.get("https://test.k6.io");
+
+  //assertions
+  check(response, {
+    "status is 200": (r) => r.status === 200,
+    "correct page is rendered": (r) =>
+      r.body.includes("Collection of simple web-pages") === true,
+  });
+}
